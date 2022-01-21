ebb3e9f7d26d011d4debe16d60bd894377483f83
— <drcmda@gmail.com>
2022-01-21 13:22:28 +0100

    first draft

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# tunnel-rat
-🐀 non gratum anus rodentum
+<center>
+    <img src="/logo.jpg" alt="Tunnel Rat">
+</center>
+
+https://codesandbox.io/s/basic-demo-forked-kxq8g
+
+```jsx
+import tunnel from 'tunnel-rat'
+
+const three = tunnel()
+const dom = tunnel()
+
+function App() {
+  return (
+    <div>
+      <Canvas>
+        <three.Out />
+        <dom.In>
+          <h1>hello</h1>
+        </dom.In>
+      </Canvas>
+
+      <three.In>
+        <mesh>
+          <sphereGeometry />
+        </mesh>
+      </three.In>
+
+      <dom.Out />
+    </div>
+  )
+}
+```
---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import React, { useLayoutEffect } from 'react'
+import create, { SetState } from 'zustand'
+
+type Props = { children: React.ReactNode }
+
+export default function tunnel() {
+  const useStore = create<{
+    current: React.ReactNode | null
+    set: SetState<{
+      current: React.ReactNode | null
+      set: any
+    }>
+  }>((set) => ({ current: null, set }))
+  return {
+    In: ({ children }: Props) => {
+      const set = useStore((state) => state.set)
+      useLayoutEffect(() => void set({ current: children }))
+      return null
+    },
+    Out: () => {
+      const current = useStore((state) => state.current)
+      return current
+    },
+  }
+}
