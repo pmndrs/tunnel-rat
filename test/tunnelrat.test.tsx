719c47d82c7bb87bd8ec37790ba65f8903b7f463
Hendrik Mans <hendrik@mans.de>
2022-08-17 12:48:52 +0200

    First actual test

--- test/tunnelrat.test.tsx
+++ test/tunnelrat.test.tsx
@@ -1,9 +1,39 @@
 import '@testing-library/jest-dom'
 import { render } from '@testing-library/react'
 import React from 'react'
+import tunnel from '../src'
 
 describe('tunnelrat', () => {
-  it('foo', () => {
-    render(<p>squeak</p>)
+  it('Transports the children of In into Out', () => {
+    const t = tunnel()
+
+    const A = () => (
+      <ul>
+        <t.Out />
+      </ul>
+    )
+
+    const B = () => (
+      <t.In>
+        <li>One</li>
+      </t.In>
+    )
+
+    const { container } = render(
+      <>
+        <A />
+        <B />
+      </>
+    )
+
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <ul>
+          <li>
+            One
+          </li>
+        </ul>
+      </div>
+    `)
   })
 })
