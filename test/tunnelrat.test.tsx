0ccfc917d3fca20ae61841df144c4b90285ec072
Hendrik Mans <hendrik@mans.de>
2022-08-17 12:45:18 +0200

    Set up tests

---
+++ test/tunnelrat.test.tsx
@@ -0,0 +1,9 @@
+import '@testing-library/jest-dom'
+import { render } from '@testing-library/react'
+import React from 'react'
+
+describe('tunnelrat', () => {
+  it('foo', () => {
+    render(<p>squeak</p>)
+  })
+})
